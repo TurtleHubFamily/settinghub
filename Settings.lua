7a10d37de82969ccfa40b115184108e1d9af39b4
Turtle Hub <102744062+TurtleHubFamily@users.noreply.github.com>
2024-03-24 13:42:02 +0700

    Add files via upload

---
+++ Settings.lua
@@ -0,0 +1,10 @@
+_G.TieuDe = "Buy Source & Script Contact Discord : @ni.tk"
+_G.ThongBao = "Rồi Okay !"
+_G.ThongBaoChoose = "Click Button Get Key To Browser Paste !"
+
+_G.Discord = "https://discord.gg/NWbJFKvw"
+_G.YouTube = "https://www.youtube.com/channel/UCPT-2Q-2XQp5B4esGA3uE4A"
+_G.TikTok = "Comming Soon !!!"
+
+
+getgenv().BanQuyen = "\n Copyright 2024 - 2024 Nguyen Tan Khang, All Rights Reserved, Copyright Protected By DCMA."
